dcff3f6e7e5aebea497213f06919c6c82ed99e20
DiogoCoutoooo <diogocoutoabrantes@gmail.com>
2025-12-24 08:15:25 +0000

    Setup inicial Astro com GSAP e Lenis

---
+++ astro.config.mjs
@@ -0,0 +1,8 @@
+// @ts-check
+import { defineConfig } from 'astro/config';
+
+// https://astro.build/config
+export default defineConfig({
+  site: 'https://diogocoutoooo.github.io',
+  base: '/', // Se for o repositório principal username.github.io
+});
